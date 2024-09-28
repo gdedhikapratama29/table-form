5e10678f0266335a38578b3dc2e2d51e0e4a5ae4
dhika <gdedhikapratamaa@gmail.com>
2024-09-28 09:17:34 +0700

    update terbaru

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express  = require('express');
 const mysql = require('mysql');
 const { title } = require('process');
 const BodyParser = require('body-parser');
+const path = require('path');
 
 
 const app = express();
 
 app.use(BodyParser.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs")
-app.set("views", "views")
 
+app.set('view engine', 'ejs')
+
+app.set('views', [
+  path.join(__dirname, 'views'),
+  path.join(__dirname, 'src')
+]); 
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -25,7 +30,56 @@ db.connect((err) => {
     if (err) throw err
     console.log('connected');
 
+    const formatDate = (date) => {
+        const d = new Date(date);
+        const day = String(d.getDate()).padStart(2, '0');
+        const month = String(d.getMonth() + 1).padStart(2, '0');
+        const year = d.getFullYear();
+        return `${day}-${month}-${year}`;
+    };
     
+    app.get("/buku", (req, res) => {
+        const sql = "SELECT * FROM buku";
+        
+        db.query(sql, (err, result) => {
+            if (err) {
+                // Jika ada error dalam kueri, tangani error di sini
+                return res.status(500).send("Terjadi kesalahan saat mengambil data.");
+            }
+            
+            // Memastikan bahwa result sudah siap sebelum render
+            let users = JSON.parse(JSON.stringify(result));
+            res.render("buku", { users: users, title: "PERPUSTAKAAN", formatDate: formatDate  });
+        });
+    });
+
+    app.post("/add", (req, res) => {
+        console.log(req.body);
+    
+        // Menggunakan prepared statements untuk mencegah SQL Injection
+        const insertSql = `INSERT INTO buku (user, tanggal_peminjaman, keterangan, nama_buku) VALUES (?, ?, ?, ?)`;
+        
+        const values = [
+            req.body.user,
+            req.body.date,
+            req.body.keterangan,
+            req.body.nama_buku
+        ];
+    
+        db.query(insertSql, values, (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send("Error occurred");
+            }
+            res.redirect("/buku");
+        });
+    });
+    
+
+   
+
+
+    // form data
     app.get("/", (req, res) => {
         const sql = "select * from user"
         db.query(sql, (err, result) => {
@@ -34,15 +88,18 @@ db.connect((err) => {
             res.render("index", {users: users, title: "FORM DATA"});
         })
     })
+    
 
+    // form data
     app.post("/tambah", (req, res) => {
-        const insertSql = `INSERT INTO user (nama, email, password, no_hp, tanggal_lahir, jenis_kelamin, upload_file) VALUES('${req.body.nama}', '${req.body.email}', '${req.body.password}', '${req.body.no_hp}', '${req.body.tanggal_lahir}', '${req.body.jenis_kelamin}', '${req.body.upload_file}');`
+        const insertSql = `INSERT INTO user (nama, email, password, no_hp, jenis_kelamin, upload_file) VALUES('${req.body.nama}', '${req.body.email}', '${req.body.password}', '${req.body.no_hp}', '${req.body.jenis_kelamin}', '${req.body.upload_file}');`
         db.query(insertSql, (err, result) => {
           if (err) throw err;
           res.redirect("/");
         })
      });
 
+    //  form data
      app.get("/edit/:id",(req,res) => {
         const user = `select * from user where id = ${req.params.id}`;
         db.query(user,(err, result) => {
@@ -52,6 +109,7 @@ db.connect((err) => {
        });
      })
 
+    //  form data
      app.post("/update/:id", (req, res) => {
         console.log('update')
         console.log(req.body)
@@ -61,7 +119,6 @@ db.connect((err) => {
                                email = '${req.body.email}', 
                                password = '${req.body.password}', 
                                no_hp = '${req.body.no_hp}', 
-                               tanggal_lahir = '${req.body.tanggal_lahir}', 
                                jenis_kelamin = '${req.body.jenis_kelamin}', 
                                upload_file = '${req.body.upload_file}'
                            WHERE id = ?`;
@@ -76,6 +133,7 @@ db.connect((err) => {
         });
     });
 
+    // form data
     app.get("/delete/:id", (req, res) => {
         const id = parseInt(req.params.id);
         console.log(id)
